137dfabb61a9fd05ed2bc0e885bb2bd1f6e6cead
naoeoneto <abdpneto@gmail.com>
2023-03-22 10:51:33 -0300

    feat: update entities

--- src/entities/contact.entity.ts
+++ src/entities/contact.entity.ts
@@ -2,8 +2,11 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from "typeorm";
+import User from "./user.entity";
 
 @Entity("contacts")
 class Contact {
@@ -16,11 +19,23 @@ class Contact {
   @Column()
   email: string;
 
+  @Column({ nullable: true })
+  second_email: string;
+
   @Column()
   phone_number: string;
 
+  @Column({ nullable: true })
+  second_phone_number: string;
+
   @CreateDateColumn()
   createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.contacts)
+  user: User;
 }
 
 export default Contact;
--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -5,8 +5,11 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from "typeorm";
+import Contact from "./contact.entity";
 
 @Entity("users")
 class User {
@@ -19,20 +22,32 @@ class User {
   @Column({ unique: true })
   email: string;
 
+  @Column({ nullable: true })
+  second_email: string;
+
   @Column()
   password: string;
-  
+
   @Column()
   phone_number: string;
 
+  @Column({ nullable: true })
+  second_phone_number: string;
+
   @CreateDateColumn()
   createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 
   @BeforeInsert()
   @BeforeUpdate()
   hashPassword() {
     this.password = hashSync(this.password, 10);
   }
+
+  @OneToMany(() => Contact, (contact) => contact.user)
+  contacts: Contact[];
 }
 
 export default User;
