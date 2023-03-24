2440cf5fe673f21c63f1cc853be97a1730fc3213
naoeoneto <abdpneto@gmail.com>
2023-03-24 10:49:29 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import express from "express";
 import { loginRouter } from "./routers/login.router";
 import { userRouter } from "./routers/user.router";
 import { contactRouter } from "./routers/contact.router";
+import cors from "cors";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/login", loginRouter);
 app.use("/users", userRouter);
-app.use('/contacts', contactRouter)
+app.use("/contacts", contactRouter);
